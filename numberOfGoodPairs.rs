b7a2e56ae5db560df26c5b0d7ca957d1ac77a07d
Victor <victor.as.nascimento@gmail.com>
2020-10-06 16:03:10 -0300

    add description

--- numberOfGoodPairs.rs
+++ numberOfGoodPairs.rs
@@ -1,3 +1,10 @@
+// This task is an easy level problem on leetCode.
+// The idea is to iterate through a list of i32's
+// and count the amount of times that the elements
+// in the i and j positions of the array are equal.
+
+// This solution runs  in 0ms and is the fastest on the platform.
+
 pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
     let mut res = 0;
     let mut i = 0;
