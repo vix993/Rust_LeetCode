efaa1bdf1b689074e0a88d11ce4fd1cb7fe9c21a
Victor <victor.as.nascimento@gmail.com>
2020-10-07 10:46:07 -0300

    shuffle_string submission

---
+++ shuffle_string/README.md
@@ -0,0 +1,10 @@
+# Shuffle The Array
+
+![Alt text](./Question.png?raw=true "Question")
+![Alt text](./Example1.png?raw=true "Examples")
+![Alt text](./Examples.png?raw=true "Examples")
+
+## Success Details
+
+![Alt text](./Success.png?raw=true "Success")
+
---
+++ shuffle_string/shuffle_string.rs
@@ -0,0 +1,30 @@
+// This is an easy level challenge on leetcode
+// The idea is to shuffle the given string array
+// in the order that is indicated in the given Vec<i32>
+// e.g. [1, 0, ...] -> "ehllo" = "e" goes in position 1
+// and "h" in position 0
+
+// As of the submission, this solution is the fastest with Rust
+
+pub fn restore_string(s: String, indices: Vec<i32>) -> String {
+    let copy: Vec<char> = s.chars().collect();
+    let mut ret: String = String::new();
+
+    for j in 0..s.len() {
+        ret.push(
+            copy[
+                indices
+                .iter()
+                .position(|&x| x == j as i32)
+                .unwrap()
+            ]
+        )
+    }
+    return ret;
+}
+
+fn main() {
+    println!("{:?}",
+             restore_string("codeleet".to_string(),
+                            [4, 5, 6, 7, 0, 2, 1, 3].to_vec()));
+}
